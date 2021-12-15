255e84e41768d25e661ffefbedece04ec21047c8
Chris Bieganek <cwbieganek@gmail.com>
2021-12-15 17:11:04 -0600

    Add Header component

---
+++ src/components/Header.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+	export let title: string = 'Application Title';
+</script>
+
+<div class='calcite-theme-dark'>
+	<h3>{title}</h3>
+</div>
+
+<style>
+	div {
+		display: flex;
+		align-items: center;
+		height: 60px;
+		background-color: var(--calcite-ui-background);
+		color: var(--calcite-ui-text-1);
+	}
+</style>
