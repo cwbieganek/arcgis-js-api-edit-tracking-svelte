32f53aa8d6dd4b1351afc5eeb25147f2d2b76c22
Chris Bieganek <cwbieganek@gmail.com>
2021-12-15 17:11:23 -0600

    Add Header

--- src/App.svelte
+++ src/App.svelte
@@ -1,8 +1,10 @@
 <script lang="ts">
 	export let name: string;
+	import Header from "./components/Header.svelte";
 </script>
 
 <main>
+	<Header title='ArcGIS API for JavaScript with Edit Tracking (Built using Svelte)' />
 	<h1>Hello {name}!</h1>
 	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
 </main>
