67ab0920810a51b20857e94606ce9f8d79edf79a
Chris Bieganek <cwbieganek@gmail.com>
2021-12-15 17:31:32 -0600

    Fix button vertical alignment

--- src/components/Header.svelte
+++ src/components/Header.svelte
@@ -2,16 +2,26 @@
 	export let title: string = 'Application Title';
 </script>
 
-<div class='calcite-theme-dark'>
+<div class='header'>
 	<h3>{title}</h3>
+	<div class='header-button-container-right'><button>Test</button></div>
 </div>
 
 <style>
-	div {
+	.header {
 		display: flex;
+		flex-direction: row;
 		align-items: center;
 		height: 60px;
 		background-color: var(--calcite-ui-background);
 		color: var(--calcite-ui-text-1);
+		padding: 0 1em;
+	}
+
+	.header-button-container-right {
+		display: flex;
+		flex: 1;
+		justify-content: end;
+		align-items: center;
 	}
 </style>
