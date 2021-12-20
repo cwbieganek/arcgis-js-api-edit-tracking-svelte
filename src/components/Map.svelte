0093f8a4dc4d91c510f6ad2adf302d06731283a9
Chris Bieganek <cwbieganek@gmail.com>
2021-12-19 19:57:00 -0600

    Make feature table re-sizable

--- src/components/Map.svelte
+++ src/components/Map.svelte
@@ -24,7 +24,7 @@
 	let loading = true;  // Controls the visibility of the loading component and the map
 
 	// Wait for component to be mounted before calling setupMapView()
-	// This is necessary because the #container element will not exist until the component mounts.
+	// This is necessary because the #map-container element will not exist until the component mounts.
 	onMount(async () => {
 		console.log('Map component has mounted.');
 		setupMapView();
@@ -33,6 +33,12 @@
 	// Creates Map, MapView, and adds widgets
 	function setupMapView(): void {
 		console.log('Setting up map view.');
+
+		// Query all resizers. Hopefully they will exist at this point.
+		document.querySelectorAll('.resizer').forEach(function (ele) {
+			resizable(ele);
+		});
+
 		const map = new Map({
 			basemap: basemap
 		});
@@ -40,7 +46,7 @@
 		// MapView is responsible for visualizing the Map and widgets, controlling zoom, extent, etc.
 		const view = new MapView({
 			map: map,
-			container: 'container',
+			container: 'map-container',
 			extent: new Extent({
 				xmin: -9177811,
 				ymin: 4247000,
@@ -190,25 +196,118 @@
 			});
 		});
 	}
+
+	// Pulled from an example here:
+	// https://htmldom.dev/create-resizable-split-views/
+	function resizable(resizer) {
+		const direction = resizer.getAttribute('data-direction') || 'horizontal';
+		const prevSibling = resizer.previousElementSibling;
+		const nextSibling = resizer.nextElementSibling;
+
+		// The current position of mouse
+		let x = 0;
+		let y = 0;
+		let prevSiblingHeight = 0;
+		let prevSiblingWidth = 0;
+
+		// Handle the mousedown event
+		// that's triggered when user drags the resizer
+		const mouseDownHandler = function (e) {
+			// Get the current mouse position
+			x = e.clientX;
+			y = e.clientY;
+			const rect = prevSibling.getBoundingClientRect();
+			prevSiblingHeight = rect.height;
+			prevSiblingWidth = rect.width;
+
+			// Attach the listeners to `document`
+			document.addEventListener('mousemove', mouseMoveHandler);
+			document.addEventListener('mouseup', mouseUpHandler);
+		};
+
+		const mouseMoveHandler = function (e) {
+				// How far the mouse has been moved
+				const dx = e.clientX - x;
+				const dy = e.clientY - y;
+
+				switch (direction) {
+						case 'vertical':
+							const h =
+									((prevSiblingHeight + dy) * 100) /
+									resizer.parentNode.getBoundingClientRect().height;
+							prevSibling.style.height = `${h}%`;
+							break;
+						case 'horizontal':
+						default:
+							const w =
+									((prevSiblingWidth + dx) * 100) / resizer.parentNode.getBoundingClientRect().width;
+							prevSibling.style.width = `${w}%`;
+							break;
+				}
+
+			const cursor = direction === 'horizontal' ? 'col-resize' : 'row-resize';
+			resizer.style.cursor = cursor;
+			document.body.style.cursor = cursor;
+
+			prevSibling.style.userSelect = 'none';
+			prevSibling.style.pointerEvents = 'none';
+
+			nextSibling.style.userSelect = 'none';
+			nextSibling.style.pointerEvents = 'none';
+		};
+
+		const mouseUpHandler = function () {
+			resizer.style.removeProperty('cursor');
+			document.body.style.removeProperty('cursor');
+
+			prevSibling.style.removeProperty('user-select');
+			prevSibling.style.removeProperty('pointer-events');
+
+			nextSibling.style.removeProperty('user-select');
+			nextSibling.style.removeProperty('pointer-events');
+
+			// Remove the handlers of `mousemove` and `mouseup`
+			document.removeEventListener('mousemove', mouseMoveHandler);
+			document.removeEventListener('mouseup', mouseUpHandler);
+		};
+
+		// Attach the handler
+		resizer.addEventListener('mousedown', mouseDownHandler);
+	};
 </script>
 
 {#if loading}
 	<calcite-loader active type="indeterminate"></calcite-loader>
 {/if}
 
-<div id='container' class='container' style={loading ? 'visibility: hidden;' : ''}></div>
-<div id='feature-table-container' class='container' style={loading ? 'visibility: hidden;' : ''}>
-	<div id='feature-table'></div>
+<div id='map-and-table-container' style={loading ? 'visibility: hidden;' : ''}>
+	<div id='map-container' class='container'></div>
+	<div class='resizer' data-direction='vertical'></div>
+	<div id='feature-table-container' class='container'>
+		<div id='feature-table'></div>
+	</div>
 </div>
 
 <style>
-	#container {
+	#map-and-table-container {
+		flex: 1;
+	}
+
+	#map-container {
 		height: 75%;
-		min-height: 50%;
+		max-height: 90%;
+		min-height: 25%;
 	}
 
 	#feature-table-container {
-		height: 25%;
-		max-height: 50%;
+		flex: 1;
+		max-height: 75%;
+	}
+
+	.resizer[data-direction='vertical'] {
+			background-color: white;
+			cursor: ns-resize;
+			height: 3px;
+			width: 100%;
 	}
 </style>
